0042bef0a659f70bea5fad95c48c982248f9af7f
Viktor <Vikman88@gmail.com>
2021-04-10 19:30:51 +0400

    ref10

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: 'index.html',
       filename: 'index.html',
       minify: {
         collapseWhitespace: isProd,
