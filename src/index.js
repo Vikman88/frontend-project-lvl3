f9b52a763c1ec5b675df9c7f5a251f28515fea29
Viktor <Vikman88@gmail.com>
2021-04-11 23:02:55 +0400

    asd6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-//import '../styles/main.scss';
-//import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-//import 'jquery';
 import init from './init.js';
 
 init();
