f19644f1c1a874defccf6019c40324fb752b63ba
Viktor <Vikman88@gmail.com>
2021-04-11 20:42:40 +0400

    asd2

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ const isProd = !isDev;
 const filename = (ext) => (isDev ? `[name].[hash].${ext}` : `[name].${ext}`);
 
 module.exports = {
-  //context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, 'src'),
   mode: isProd ? 'production' : 'development',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: filename('js'),
@@ -39,7 +39,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'RSS aggregator',
-      template: 'index.html',
+      template: './index.html',
     }),
   ],
 };
